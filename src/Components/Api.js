7d280c0d4036fdd7305bf637abdfcf6e238e404c
Ugne <ugne.kryzeviciute@gmail.com>
2017-12-30 21:08:32 +0200

    Delete comments

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,5 +1,4 @@
 import config from './config'
-// import axios from 'axios'
 
 const api = {
   getUrl: (pageNumber) => {
--- src/Components/ShotList.js
+++ src/Components/ShotList.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import Shot from './Shot';
+import PropTypes from 'prop-types';
 
-class NewShotList extends React.Component {
+class ShotList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      shots: [],
-      pageNumber: 0,
-      loaded: false,
+      loaded: 0,
     }
+    this.handleImageLoaded = this.handleImageLoaded.bind(this)
   }
+
+  handleImageLoaded() {
+    this.setState({
+      loaded: ++this.state.loaded
+    })
+  }
+
+  // componentDidUpdate(nextProps) {
+  //   if (this.props.data.length === this.state.loaded) {
+  //       this.props.handleImageLoaded(this.state.loaded)
+  //   }
+  // }
 
   render() {
     return (
-      <div className="gallery">
+      <div className="gallery" >
         {this.props.data.map(shot =>
           <Shot
+            ref="shotElement"
             url={shot.images.normal}
             title={shot.title}
             author={shot.user.name}
             key={shot.id}
             id={shot.id}
-            handleImageLoaded={this.props.onAllShotsLoaded}
-            // imageStatus={this.props.allLoaded}
+            handleImageLoaded={this.handleImageLoaded}
+            imageStatus={this.props.imageStatus}
             className="image-wrapper mobile-visible"
           />
         )}
@@ -32,5 +45,5 @@ class NewShotList extends React.Component {
   }
 }
 
-export default NewShotList
+export default ShotList
 
